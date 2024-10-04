023aae62c2853fb98acab768c879a4f15e5dd3e2
Mankaz01 <20047409@mydbs.ie>
2024-10-04 14:31:02 +0100

    added

--- arrOne.js
+++ arrOne.js
@@ -1,17 +1,18 @@
-function sumOfMul(a, b, l){
-    let sum = 0
-    for(let i = o; i <l.length; i++){
-        if(l[i] % a === 0 || l[i] % b === 0){
-            sum +=l[i]
+function sumOfMul(a, b, l) {
+    let sum = 0;
+    for (let i = 0; i < l.length; i++) {
+        if (l[i] % a === 0 || l[i] % b === 0) {
+            sum += l[i];
         }
     }
     return sum;
 }
 
-function multipleSum(){
+function multipleSum() {
     let a = parseInt(document.getElementById("a").value);
     let b = parseInt(document.getElementById("b").value);
     let l = document.getElementById("l").value.split(',').map(Number);
+    
     let result = sumOfMul(a, b, l);
-    alert("sum is: " + result)    
+    alert("Sum is: " + result);
 }
