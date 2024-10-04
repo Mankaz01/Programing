b4142372908f0e91d77b5fa73b894c43da6df661
Mankaz01 <20047409@mydbs.ie>
2024-10-04 14:46:28 +0100

    hardcode

--- arrOne.js
+++ arrOne.js
@@ -8,11 +8,15 @@ function sumOfMul(a, b, l) {
     return sum;
 }
 
-function multipleSum() {
-    let a = parseInt(document.getElementById("a").value);
-    let b = parseInt(document.getElementById("b").value);
-    let l = document.getElementById("l").value.split(',').map(Number);
+// function multipleSum() {
+//     let a = parseInt(document.getElementById("a").value);
+//     let b = parseInt(document.getElementById("b").value);
+//     let l = document.getElementById("l").value.split(',').map(Number);
     
-    let result = sumOfMul(a, b, l);
-    alert("Sum is: " + result);
-}
+//     let result = sumOfMul(a, b, l);
+//     alert("Sum is: " + result);
+// }
+const a = 5;
+const b = 2;
+const l = [1, 4, 6, 10];
+alert(sumOfMul(a, b, l))
