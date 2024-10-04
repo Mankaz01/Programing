1fcd6e497944e0db557ae62722f654c3bde35516
Mankaz01 <20047409@mydbs.ie>
2024-10-04 14:37:05 +0100

    arrTwo

---
+++ arrTwo.js
@@ -0,0 +1,18 @@
+function sumOfMul(a, b, l) {
+    let sum = 0;
+    for (let i = 0; i < l.length; i++) {
+        if (l[i] % a[0] === 0 || l[i] % a[1] === 0) {
+            sum += l[i];
+        }
+    }
+    return sum;
+}
+
+function multipleSum() {
+    let a = [parseInt(document.getElementById("a").value),
+    parseInt(document.getElementById("b").value)];
+    let l = document.getElementById("l").value.split(',').map(Number);
+
+    let result = sumOfMul(a, b, l);
+    alert("Sum is: " + result);
+}
