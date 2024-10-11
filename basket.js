8a6c6ac0aa7e8674610978774e593ce3d2eab295
Mankaz01 <20047409@mydbs.ie>
2024-10-11 13:09:11 +0100

    basket

---
+++ basket.js
@@ -0,0 +1,26 @@
+function totalCost(basket,price){
+    let totalCost = 0
+    for(let product in basket)
+    {
+        if(price.hasOwnProperty(product))
+            totalCost = totalCost + (basket[product]*price[product])
+    }
+    return parseFloat(totalCost.toFixed(2));
+}
+
+const basket ={
+    "apple" : 4,
+    "mango" : 3,
+    "bannana" : 2
+}
+
+
+const price ={
+    "apple" : 0.40,
+    "mango" : 0.70,
+    "bannana" : 0.65
+}
+
+result = totalCost(basket,price)
+
+alert(result)
