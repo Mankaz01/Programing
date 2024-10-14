72757df06c3acc6323370131fc7f28c8aff8a3aa
Mankaz01 <20047409@mydbs.ie>
2024-10-14 10:28:47 +0100

    working

--- arrThree.js
+++ arrThree.js
@@ -3,9 +3,7 @@ const l = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
  function calsum(){
      let sum = 0;
-     
-alert('working');
-for (let i = 0; i < l.length; i++) {
+    for (let i = 0; i < l.length; i++) {
     let num =l[i];
     for (let j = 0; j < a.length; j++) {
         if (num % a[j] === 0) {
@@ -14,8 +12,6 @@ for (let i = 0; i < l.length; i++) {
         }
     }
 }
-console.log("Sum of multiples:", ${sum});
 alert('sum of multiple:'+ sum)
 }
-
 window.onload=calsum;
